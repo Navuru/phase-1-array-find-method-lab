4947167bc548c4a9b185dae64795682b5e337f41
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-08-21 23:30:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// // code your solution here
+// function superbowlWin (record) {
+//     const result = record.find(record);
+//         return record.result === "W";
+//         }
+
+// superbowlWin = (record) => {
+
+//     const result = record.find( record => record.result === "W" );
+    
+//   //  ✔ returns undefined if the record has no win objects
+//     return !!result ? result.year : undefined;
+// }
+// function result() {
+//     record.find(function (){
+//         record.result === "W";
+//     })
+// }
+
+// function superbowlWin(record){
+//     return result();
+// }
+// // function superbowlWin(record) {
+//     record.find(result);
+//     return ;
+// }
+
+function superbowlWin (record) {
+    const result = record.find( record => record.result === "W" );
+
+    return !!result ? result.year : undefined;
+}
